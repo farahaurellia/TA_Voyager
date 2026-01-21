c5cc4571568b945de31c3f1a67015a1154ac4e72
farahaurellia <farahaurelliaa@gmail.com>
2026-01-21 10:24:00 +0700

    update openai_api_key placeholder in run_voyager.py

--- run_voyager.py
+++ run_voyager.py
@@ -3,7 +3,7 @@ from voyager import Voyager
 # Ganti angka di bawah dengan port yang muncul di Minecraft kamu
 voyager = Voyager(
     mc_port=51234, 
-    openai_api_key="",
+    openai_api_key=" YOUR_API_KEY_HERE ",
     action_agent_model_name="gpt-4o-mini",  
     critic_agent_model_name="gpt-4o-mini"
 )
